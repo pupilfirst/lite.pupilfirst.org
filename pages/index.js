49eb898d5d337849ece6e35314a9566b9ef7085b
vinu tv <vinutv91@gmail.com>
2021-06-09 00:43:29 +0530

    Fix broken button style

--- pages/index.js
+++ pages/index.js
@@ -127,13 +127,13 @@ export default function Home() {
               </div>
             </div>
           </div>
-          <div class="absolute inset-0 md:inset-x-0 md:top-8 2xl:inset-0 z-0">
+          <div className="absolute inset-0 md:inset-x-0 md:top-8 2xl:inset-0 z-0">
             <img
-              class="h-full w-full object-cover relative"
+              className="h-full w-full object-cover relative"
               src="hero/hero-bg.png"
               alt="LITE - hero background image"
             />
-            <div class="absolute inset-0"></div>
+            <div className="absolute inset-0"></div>
           </div>
         </section>
         <section className="mx-4 xl:mx-0 pb-10 md:pb-16">
@@ -216,27 +216,27 @@ export default function Home() {
                   All India Council for Technical Education.
                 </p>
                 <a
-                  className="inline-flex items-center px-4 py-3 rounded-lg space-x-2 font-semibold bg-primary-100 text-primary-800 mt-8 border border-primary-400 hover:bg-primary-50 hover:shadow-lg transition"
+                  className="inline-flex items-center px-4 py-3 rounded-lg space-x-2 font-semibold bg-primary-100 text-primary-800 mt-4 md:mt-6 border border-primary-400 hover:bg-primary-50 hover:shadow-lg outline-none focus:ring focus:ring-primary-200 focus:ring-offset-1 transition"
                   href="letter-from-VCM/Letter-from-the-VCM-AICTE.pdf"
                   target="_blank"
                 >
-                  <span>
+                  <div>
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
                       width="16"
                       height="16"
                       fill="currentColor"
-                      class="w-full h-7 text-primary-500"
+                      className="w-6 h-6 text-primary-500"
                       viewBox="0 0 16 16"
                     >
                       <path d="M5.523 12.424c.14-.082.293-.162.459-.238a7.878 7.878 0 0 1-.45.606c-.28.337-.498.516-.635.572a.266.266 0 0 1-.035.012.282.282 0 0 1-.026-.044c-.056-.11-.054-.216.04-.36.106-.165.319-.354.647-.548zm2.455-1.647c-.119.025-.237.05-.356.078a21.148 21.148 0 0 0 .5-1.05 12.045 12.045 0 0 0 .51.858c-.217.032-.436.07-.654.114zm2.525.939a3.881 3.881 0 0 1-.435-.41c.228.005.434.022.612.054.317.057.466.147.518.209a.095.095 0 0 1 .026.064.436.436 0 0 1-.06.2.307.307 0 0 1-.094.124.107.107 0 0 1-.069.015c-.09-.003-.258-.066-.498-.256zM8.278 6.97c-.04.244-.108.524-.2.829a4.86 4.86 0 0 1-.089-.346c-.076-.353-.087-.63-.046-.822.038-.177.11-.248.196-.283a.517.517 0 0 1 .145-.04c.013.03.028.092.032.198.005.122-.007.277-.038.465z" />
                       <path
-                        fill-rule="evenodd"
+                        fillRule="evenodd"
                         d="M4 0h5.293A1 1 0 0 1 10 .293L13.707 4a1 1 0 0 1 .293.707V14a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2zm5.5 1.5v2a1 1 0 0 0 1 1h2l-3-3zM4.165 13.668c.09.18.23.343.438.419.207.075.412.04.58-.03.318-.13.635-.436.926-.786.333-.401.683-.927 1.021-1.51a11.651 11.651 0 0 1 1.997-.406c.3.383.61.713.91.95.28.22.603.403.934.417a.856.856 0 0 0 .51-.138c.155-.101.27-.247.354-.416.09-.181.145-.37.138-.563a.844.844 0 0 0-.2-.518c-.226-.27-.596-.4-.96-.465a5.76 5.76 0 0 0-1.335-.05 10.954 10.954 0 0 1-.98-1.686c.25-.66.437-1.284.52-1.794.036-.218.055-.426.048-.614a1.238 1.238 0 0 0-.127-.538.7.7 0 0 0-.477-.365c-.202-.043-.41 0-.601.077-.377.15-.576.47-.651.823-.073.34-.04.736.046 1.136.088.406.238.848.43 1.295a19.697 19.697 0 0 1-1.062 2.227 7.662 7.662 0 0 0-1.482.645c-.37.22-.699.48-.897.787-.21.326-.275.714-.08 1.103z"
                       />
                     </svg>
-                  </span>
-                  <span>Read the full letter</span>
+                  </div>
+                  <div>Read the full letter</div>
                 </a>
               </div>
             </div>
@@ -578,14 +578,14 @@ export default function Home() {
                     </div>
                   </div>
                 </div>
-                <div class="flex items-start lg:items-center space-x-3 bg-yellow-50 border border-yellow-400 px-4 py-3 mt-4 rounded-lg text-sm ">
+                <div className="flex items-start lg:items-center space-x-3 bg-yellow-50 border border-yellow-400 px-4 py-3 mt-4 rounded-lg text-sm ">
                   <div>
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
                       width="16"
                       height="16"
                       fill="currentColor"
-                      class="text-primary-400 w-9 h-9"
+                      className="text-primary-400 w-9 h-9"
                       viewBox="0 0 16 16"
                     >
                       <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2z" />
@@ -750,11 +750,11 @@ export default function Home() {
                       width="16"
                       height="16"
                       fill="currentColor"
-                      class="text-primary-700"
+                      className="text-primary-700"
                       viewBox="0 0 16 16"
                     >
                       <path
-                        fill-rule="evenodd"
+                        fillRule="evenodd"
                         d="M8 4a.5.5 0 0 1 .5.5v5.793l2.146-2.147a.5.5 0 0 1 .708.708l-3 3a.5.5 0 0 1-.708 0l-3-3a.5.5 0 1 1 .708-.708L7.5 10.293V4.5A.5.5 0 0 1 8 4z"
                       />
                     </svg>
@@ -774,11 +774,11 @@ export default function Home() {
                       width="16"
                       height="16"
                       fill="currentColor"
-                      class="text-secondary-700"
+                      className="text-secondary-700"
                       viewBox="0 0 16 16"
                     >
                       <path
-                        fill-rule="evenodd"
+                        fillRule="evenodd"
                         d="M8 4a.5.5 0 0 1 .5.5v5.793l2.146-2.147a.5.5 0 0 1 .708.708l-3 3a.5.5 0 0 1-.708 0l-3-3a.5.5 0 1 1 .708-.708L7.5 10.293V4.5A.5.5 0 0 1 8 4z"
                       />
                     </svg>
@@ -862,9 +862,9 @@ export default function Home() {
                 <div className="space-y-10 sm:space-y-0 sm:grid sm:grid-cols-3 sm:gap-4 md:gap-8 lg:gap-12 py-12 lg:pb-16">
                   <ImageWithDescription
                     name="Hari Gopal"
-                    caption={<>Chief Technology Officer, Pupilfirst</>}
+                    caption={<p>Chief Technology Officer, Pupilfirst</p>}
                     role={
-                      <>
+                      <div>
                         <div className="pb-2 font-semibold">
                           Course Author &amp; Coach
                         </div>{" "}
@@ -873,7 +873,7 @@ export default function Home() {
                         development of the Pupilfirst LMS, an open-source
                         project that blends a traditional stack (RoR) with the
                         latest and greatest tech (ReScript).
-                      </>
+                      </div>
                     }
                     imgSrc="people/hari-gopal.png"
                   />
