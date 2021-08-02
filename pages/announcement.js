5305b501d63e824ae3e6933d6572bc60bb0e6514
Reena Singh <reena@pupilfirst.org>
2021-08-02 16:46:38 +0530

    More minor updates  (#9)
    * Update index.js
    
    * Update announcement.js

--- pages/announcement.js
+++ pages/announcement.js
@@ -3493,7 +3493,7 @@ export default function Home() {
                       LITE Welcome Kit
                     </h3>
                     <p className="pt-2 text-sm lg:text-lg">
-                      The welcome kit for institutions containing instructions on next steps can be downloaded here. Please note last date for sending copy of signed MoU is Aug 13,2021
+                      The Welcome Kit has details on the next steps for Institutions. Please download and do the needful before August 13, 2021.
                     </p>
                     <a
                       href="https://drive.google.com/drive/folders/19sa4KQHw7FyU3ez1l7IFbt5czNU0t84h?usp=sharing"
