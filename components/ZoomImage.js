4b56c4a268f5569ec1c594034c059ba580b30f44
Raj Suhail <rajsuhailmalik@gmail.com>
2022-10-20 15:21:12 +0530

    Remove styles from img

--- components/ZoomImage.js
+++ components/ZoomImage.js
@@ -14,10 +14,9 @@ export default function ZoomImage({ src, alt, className = " " }) {
         onClick={() => setIsZoomed((prev) => !prev)}
         className={`${className} rounded-lg ${
           isZoomed
-            ? "w-full relative top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2  object-cover cursor-zoom-out "
+            ? "w-11/12 relative top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2  object-cover cursor-zoom-out "
             : "w-full h-full object-cover cursor-zoom-in"
         }`}
-        style={isZoomed ? { padding: "20px", width: "90%" } : { width: "100%" }}
         src={src}
         alt={alt}
       />
