b976a78946cfcdf5d5e4e4533e90438743107a0c
Nagarajan Gunasekaran <108739037+Nagarajan15@users.noreply.github.com>
2023-01-12 11:03:05 +0530

    Update FDP Stats (#50)

--- pages/wd-fdp.js
+++ pages/wd-fdp.js
@@ -245,7 +245,7 @@ export default function Page() {
                     <p>States</p>
                   </div>
                   <div className="flex flex-col justify-center xl:justify-start items-center bg-white bg-opacity-25 xl:bg-transparent backdrop-blur-sm drop-shadow-sm xl:drop-shadow-none rounded-lg text-secondary-600 p-2 leading-snug">
-                    <p className="text-xl md:text-4xl font-bold">98</p>
+                    <p className="text-xl md:text-4xl font-bold">103</p>
                     <p>LITE MoU Received</p>
                   </div>
                 </div>
